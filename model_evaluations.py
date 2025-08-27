44ff6855f82661d9981fa56d43735de2ce597186
Timure228 <arductimur@gmail.com>
2025-08-27 14:50:22 +0200

    del: custom parameter is deleted feat: preprocessing function added

--- model_evaluations.py
+++ model_evaluations.py
@@ -4,28 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import shutil
+import glob
+from PIL import Image
 
 from model import X_test, y_test, celeba_csv
 from model_resnet_34 import ResidualUnit
-from PIL import Image
-import glob
 
 # Load the model
 model = keras.models.load_model("models/model_cnn.keras")
+
+
 # res_net_34 = keras.models.load_model("models/res_net_34.keras")
 # mobilenet = keras.models.load_model("models/MobileNetV3_Small.keras")
 
-def predict_img(model: keras.models.Model, image=None, image_path=None, plot_img=False, as_int=False, custom=False):
-    if image and image_path is None:
-        return "No image provided"
-    # Custom image preprocessing
-    if custom and image_path is not None:
-        image = Image.open(image_path)
-        image_tensor = tf.convert_to_tensor(img) / 255
-        image_tensor_resized = keras.layers.Resizing(height=128, width=128,
-                                                     crop_to_aspect_ratio=True)(image_tensor)
+def image_preprocessing(image_path):
+    image = Image.open(image_path)
+    image_tensor = tf.convert_to_tensor(image) / 255
+    image_tensor_resized = keras.layers.Resizing(height=128, width=128,
+                                                 crop_to_aspect_ratio=True)(image_tensor)
+    return image_tensor_resized
+
+
+def predict_img(model: keras.models.Model, image, plot_img=False, as_int=False):
     # Make a prediction
-    y_pred = model.predict(tf.expand_dims(image_tensor_resized, 0))
+    y_pred = model.predict(tf.expand_dims(image, 0))
     # List of the attributes
     attr_list = list(celeba_csv)[1:]
     # Turn into percents
@@ -145,4 +147,4 @@ def printout_prf(model, labels, samples):
 #                   save_path="images/blond_people")
 
 # print(predict_img(model, custom=True, image_path="example.jpg", plot_img=True))
-# print(describe(model, img_tensor_resized))
+print(describe(model, image_preprocessing("obama.jpg")))
